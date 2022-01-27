aaeb549dca2392294db2e6ee6cc1a6fea7a7e90e
Alan <tsdchaos@gmail.com>
2022-01-26 20:17:12 -0500

    here is your car

--- src/com/Syntax/Main.java
+++ src/com/Syntax/Main.java
@@ -4,10 +4,12 @@ public class Main {
     public static void main(String[] args) {
         //the basic structure
         carBasics car = new carBasics();
-        car.color="red";
-        car.number="4567";
-        car.engine="5434cc";
-        car.door="2";
+
+        car.color = "blue";
+        car.engine = "885556";
+        car.door = "4";
+        car.number = "234689";
+
         System.out.println(car.color);
     }
 }
--- src/com/Syntax/carBasics.java
+++ src/com/Syntax/carBasics.java
@@ -1,8 +1,15 @@
 package com.Syntax;
 
 public class carBasics {
+<<<<<<< Updated upstream
     String color;
     String engine;
     String door;
     String number;
+=======
+    public String color;
+    public String engine;
+    public String door;
+    public String number;
+>>>>>>> Stashed changes
 }
